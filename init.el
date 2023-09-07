58761832d1cdedd24d442c12c0f874d8e2626f5e
Jake <jake.gonzalez@ttu.edu>
2023-09-06 23:13:12 -0500

    Org file example

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (setq inhibit-startup-message t)
 
+(defvar emacs/default-font-size 180)
+
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
@@ -10,7 +12,13 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :height 150)
+(set-face-attribute 'default nil :font "Fira Code Retina" :height emacs/default-font-size)
+
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 180)
+
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 190 :weight 'regular)
 
 (load-theme 'doom-dracula t)
 
@@ -40,17 +48,15 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-
 ;; Add line numbers
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
-;; Disable line numbers ofr some modes
+;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
 		term-mode-hook
 		eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-number-mode 0))))
-
+  (add-hook mode (lambda () (display-line-number-mode nil))))
 
 (use-package command-log-mode)
 
@@ -164,6 +170,7 @@
   "gs" '(magit-status :which-key "git status"))
 
 (emacs/leader-keys
+  "k"  '(:ignore t :which-key "kill")
   "kb" '(kill-buffer-and-window :which-key "kill buffer")) 
 
 (use-package projectile
@@ -190,3 +197,55 @@
 (setq auth-sources '("~/.authinfo"))
 
 (use-package forge)
+
+(defun emacs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(defun emacs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+		  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . emacs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (emacs/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(use-package visual-fill-column
+  :hook (org-mode . emacs/org-mode-visual-fill))
+
+(defun emacs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 150
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
